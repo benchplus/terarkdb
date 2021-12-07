e31c2fd414ff93c9268c3841504b51240257c025
Orca <orca.zhang@yahoo.com>
2021-12-08 06:57:21 +0800

    :elephant: remove submodule

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-go env
-
-export CGO_CFLAGS="-I$PWD/third-party/terarkdb/output/output/include"
-export CGO_LDFLAGS="-L$PWD/third-party/terarkdb/output/lib -Wl,-Bstatic -lrocksdb -lbz2 -ljemalloc -llz4 -lsnappy -lz -lzstd -Wl,-Bdynamic -lstdc++ -lm -pthread -lgomp -lrt -ldl -laio"
-
-go get github.com/ez8-lab/gorocksdb
-go test -bench 'Benchmark' | tee output.txt
