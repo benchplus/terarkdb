a1fd2a2be4cffa8c4b3bc0b85755f8ac8d3cd699
github-action-benchmark <github@users.noreply.github.com>
2021-12-08 10:56:42 +0000

    add terarkdb (go) benchmark result for 1a76eff877e4dbf2da2e6a0792acfb829e32a2ee

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,6 +1,6 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1638916491375,
-  "repoUrl": "https://github.com/orcastor/orcas-engine-terarkdb",
+  "lastUpdate": 1638961002046,
+  "repoUrl": "https://github.com/benchplus/terarkdb",
   "entries": {
     "terarkdb": [
       {
@@ -46,6 +46,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkRandDel",
             "value": 6405,
+            "unit": "ns/op",
+            "extra": "300000 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "orca.zhang@yahoo.com",
+            "name": "Orca",
+            "username": "orca-zhang"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1a76eff877e4dbf2da2e6a0792acfb829e32a2ee",
+          "message": "🦉 add result link",
+          "timestamp": "2021-12-08T12:31:20+08:00",
+          "tree_id": "5c7128cf36911e0109ce9af9092b40e52fd6751d",
+          "url": "https://github.com/orca-zhang/terarkdb/commit/1a76eff877e4dbf2da2e6a0792acfb829e32a2ee"
+        },
+        "date": 1638961001543,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkSequenceWrite",
+            "value": 24832,
+            "unit": "ns/op",
+            "extra": "200000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkRandWrite",
+            "value": 35999,
+            "unit": "ns/op",
+            "extra": "200000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkRandRead",
+            "value": 16458,
+            "unit": "ns/op",
+            "extra": "200000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkRandDel",
+            "value": 5112,
             "unit": "ns/op",
             "extra": "300000 times\n2 procs"
           }
