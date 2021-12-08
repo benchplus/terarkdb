1416d44a42755671ca17eb92f5046a508ae8d8f7
github-action-benchmark <github@users.noreply.github.com>
2021-12-08 11:31:21 +0000

    add terarkdb (go) benchmark result for d84dd5075f612d4911857b596c9ffb7ecf70ffde

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1638961002046,
+  "lastUpdate": 1638963081566,
   "repoUrl": "https://github.com/benchplus/terarkdb",
   "entries": {
     "terarkdb": [
@@ -94,6 +94,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkRandDel",
             "value": 5112,
+            "unit": "ns/op",
+            "extra": "300000 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "orca.zhang@yahoo.com",
+            "name": "Orca",
+            "username": "orca-zhang"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d84dd5075f612d4911857b596c9ffb7ecf70ffde",
+          "message": "🦄fix url",
+          "timestamp": "2021-12-08T19:23:51+08:00",
+          "tree_id": "aa38d56fa7ce8cbcfb6b3d8f418ee0fd150c9e5a",
+          "url": "https://github.com/benchplus/terarkdb/commit/d84dd5075f612d4911857b596c9ffb7ecf70ffde"
+        },
+        "date": 1638963081035,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkSequenceWrite",
+            "value": 24008,
+            "unit": "ns/op",
+            "extra": "200000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkRandWrite",
+            "value": 37144,
+            "unit": "ns/op",
+            "extra": "200000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkRandRead",
+            "value": 16189,
+            "unit": "ns/op",
+            "extra": "100000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkRandDel",
+            "value": 5454,
             "unit": "ns/op",
             "extra": "300000 times\n2 procs"
           }
