c70f7f456392cccb17f4afa120b61adc18de6e22
Orca <orca.zhang@yahoo.com>
2021-12-09 21:00:41 +0800

    :joy_cat: add jemalloc

--- build_rocksdb.sh
+++ build_rocksdb.sh
@@ -12,7 +12,7 @@ cd $OUTPUT
 
 sudo apt-get install libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev -y
 
-cmake ../ -DCMAKE_INSTALL_PREFIX=output -DCMAKE_BUILD_TYPE=Release -DWITH_SNAPPY=ON -DWITH_LZ4=ON -DWITH_ZLIB=ON -DWITH_ZSTD=ON -DROCKSDB_BUILD_SHARED=OFF
+cmake ../ -DCMAKE_INSTALL_PREFIX=output -DCMAKE_BUILD_TYPE=Release -DWITH_JEMALLOC=ON -DWITH_SNAPPY=ON -DWITH_LZ4=ON -DWITH_ZLIB=ON -DWITH_ZSTD=ON -DROCKSDB_BUILD_SHARED=OFF
 make -j $(nproc)
 make install
 
