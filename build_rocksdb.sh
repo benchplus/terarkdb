0d9a2d428e5db2057855a3f0b82c43d8d49604f3
Orca <orca.zhang@yahoo.com>
2021-12-09 21:23:46 +0800

    :dragon_face: fix

--- build_rocksdb.sh
+++ build_rocksdb.sh
@@ -10,9 +10,10 @@ mkdir -p $OUTPUT
 cd third-party/terarkdb/third-party/jemalloc
 
 bash autogen.sh
-CFLAGS=-fPIC CXXFLAGS=-fPIC LDFLAGS=-fPIC ./configure --prefix=$OUTPUT --enable-prof
+CFLAGS=-fPIC CXXFLAGS=-fPIC LDFLAGS=-fPIC ./configure --enable-prof
 make -j $(nproc)
 make install
+ldconfig
 
 cd $OUTPUT
 
