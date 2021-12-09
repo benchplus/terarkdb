dc5cec6a4704a5d1f47f1841d3f4d4222a8c3433
Orca <orca.zhang@yahoo.com>
2021-12-10 00:32:08 +0800

    :yellow_heart: fix

--- build.sh
+++ build.sh
@@ -5,14 +5,12 @@ BASE=$PWD
 git submodule update --init --recursive
 
 cd third-party/terarkdb
-
 git remote add mine https://github.com/ez8-lab/terarkdb.git
 git pull mine dev.1.4
 
 OUTPUT=$BASE/third-party/terarkdb/output
 mkdir -p $OUTPUT
 cd $OUTPUT
-
 cmake ../ -DCMAKE_INSTALL_PREFIX=output -DCMAKE_BUILD_TYPE=Release -DWITH_TESTS=OFF -DWITH_TERARK_ZIP=OFF -DFORCE_SSE42=ON
 make -j $(nproc)
 make install
--- build_rocksdb.sh
+++ build_rocksdb.sh
@@ -3,22 +3,18 @@
 BASE=$PWD
 
 git submodule update --init --recursive
-
-OUTPUT=$BASE/third-party/rocksdb/output
-mkdir -p $OUTPUT
+sudo apt-get install libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev -y
 
 cd third-party/terarkdb/third-party/jemalloc
-
 bash autogen.sh
 CFLAGS=-fPIC CXXFLAGS=-fPIC LDFLAGS=-fPIC ./configure --enable-prof
 make -j $(nproc)
 sudo make install
 sudo ldconfig
 
+OUTPUT=$BASE/third-party/rocksdb/output
+mkdir -p $OUTPUT
 cd $OUTPUT
-
-sudo apt-get install libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev -y
-
 cmake ../ -DCMAKE_INSTALL_PREFIX=output -DCMAKE_BUILD_TYPE=Release -DWITH_JEMALLOC=ON -DWITH_SNAPPY=ON -DWITH_LZ4=ON -DWITH_ZLIB=ON -DWITH_ZSTD=ON -DROCKSDB_BUILD_SHARED=OFF
 make -j $(nproc)
 make install
