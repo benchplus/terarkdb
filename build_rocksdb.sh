d997ad49e7eacf3e5d859f0af25044f3030bcc0e
Orca <orca.zhang@yahoo.com>
2021-12-09 20:00:25 +0800

    :rabbit: fix

--- build_rocksdb.sh
+++ build_rocksdb.sh
@@ -12,8 +12,8 @@ cd $OUTPUT
 
 sudo apt-get install libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev -y
 
-cmake ../ -DCMAKE_INSTALL_PREFIX=output -DCMAKE_BUILD_TYPE=Release
-make static_lib -j $(nproc)
+cmake ../ -DCMAKE_INSTALL_PREFIX=output -DCMAKE_BUILD_TYPE=Release -DWITH_SNAPPY=ON -DWITH_LZ4=ON -DWITH_ZLIB=ON -DWITH_ZSTD=ON
+make -j $(nproc)
 make install
 
 cd $BASE
