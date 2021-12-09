43460f9039b31ff8d463007b95a872faf350743f
Orca <orca.zhang@yahoo.com>
2021-12-09 21:48:12 +0800

    :icecream: fix

--- build_rocksdb.sh
+++ build_rocksdb.sh
@@ -10,10 +10,10 @@ mkdir -p $OUTPUT
 cd third-party/terarkdb/third-party/jemalloc
 
 bash autogen.sh
-CFLAGS=-fPIC CXXFLAGS=-fPIC LDFLAGS=-fPIC ./configure --enable-prof
+CFLAGS=-fPIC CXXFLAGS=-fPIC LDFLAGS=-fPIC ./configure --prefix=$OUTPUT --enable-prof
 make -j $(nproc)
-sudo make install
-sudo ldconfig
+make install
+ldconfig
 
 cd $OUTPUT
 
@@ -22,7 +22,5 @@ sudo apt-get install libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-de
 cmake ../ -DCMAKE_INSTALL_PREFIX=output -DCMAKE_BUILD_TYPE=Release -DWITH_JEMALLOC=ON -DWITH_SNAPPY=ON -DWITH_LZ4=ON -DWITH_ZLIB=ON -DWITH_ZSTD=ON -DROCKSDB_BUILD_SHARED=OFF
 make -j $(nproc)
 make install
-
-cp $OUTPUT/output/lib/librocksdb.a $OUTPUT/lib/librocksdb.a
 
 cd $BASE
