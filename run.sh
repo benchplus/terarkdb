8d4913c8e9f7ba4b947003e8f22d4472dde567af
Orca <orca.zhang@yahoo.com>
2021-12-10 03:09:42 +0800

    :bouquet: fix for terark_zip

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 go env
 
 export CGO_CFLAGS="-I$PWD/third-party/terarkdb/output/output/include"
-export CGO_LDFLAGS="-L$PWD/third-party/terarkdb/output/lib -Wl,-Bstatic -lrocksdb -lbz2 -ljemalloc -llz4 -lsnappy -lz -lzstd -Wl,-Bdynamic -lstdc++ -lm -pthread -lgomp -lrt -ldl -laio"
+export CGO_LDFLAGS="-L$PWD/third-party/terarkdb/output/lib -Wl,-Bstatic -lrocksdb -lbz2 -ljemalloc -llz4 -lsnappy -lz -lzstd -lterark-zip-r -lboost_system -lboost_filesystem -lboost_fiber -lboost_thread -Wl,-Bdynamic -lstdc++ -lm -pthread -lgomp -lrt -ldl -laio"
 
 go get github.com/ez8-lab/gorocksdb
 go test -bench 'Benchmark' | tee output.txt
