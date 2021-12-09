101727d94e512f3d8e2e180d375f1d6c96bbc8e1
Orca <orca.zhang@yahoo.com>
2021-12-09 23:20:31 +0800

    :angel: fix

--- run_rocksdb.sh
+++ run_rocksdb.sh
@@ -4,5 +4,5 @@ go env
 export CGO_CFLAGS="-I$PWD/third-party/rocksdb/output/output/include"
 export CGO_LDFLAGS="-L$PWD/third-party/rocksdb/output/output/lib -Wl,-Bstatic -lrocksdb -lbz2 -ljemalloc -llz4 -lsnappy -lz -lzstd -Wl,-Bdynamic -lstdc++ -lm -pthread -lgomp -lrt -ldl"
 
-go get github.com/ez8-lab/gorocksdb
+go get github.com/huxinhuxin/gorocksdb
 go test -bench 'Benchmark' | tee output.txt
