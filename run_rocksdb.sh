18bd3803d646ce6b32b1b3cb536c13d4f2dbd672
Orca <orca.zhang@yahoo.com>
2021-12-10 01:27:01 +0800

    :bus: add go mod

--- run_rocksdb.sh
+++ run_rocksdb.sh
@@ -4,5 +4,6 @@ go env
 export CGO_CFLAGS="-I$PWD/third-party/rocksdb/output/output/include"
 export CGO_LDFLAGS="-L$PWD/third-party/rocksdb/output/output/lib -Wl,-Bstatic -lrocksdb -lbz2 -ljemalloc -llz4 -lsnappy -lz -lzstd -Wl,-Bdynamic -lstdc++ -lm -pthread -lgomp -lrt -ldl"
 
+go mod init
 go get github.com/ez8-lab/gorocksdb@rocksdb
 go test -bench 'Benchmark' | tee output.txt
