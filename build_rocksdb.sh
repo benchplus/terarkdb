03f2dd4c86b96a56e2804fe60a043d44aaddda91
Orca <orca.zhang@yahoo.com>
2021-12-09 20:25:18 +0800

    :evergreen_tree: fix

--- build_rocksdb.sh
+++ build_rocksdb.sh
@@ -13,7 +13,9 @@ cd $OUTPUT
 sudo apt-get install libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev -y
 
 cmake ../ -DCMAKE_INSTALL_PREFIX=output -DCMAKE_BUILD_TYPE=Release -DWITH_SNAPPY=ON -DWITH_LZ4=ON -DWITH_ZLIB=ON -DWITH_ZSTD=ON
-make -j $(nproc)
+make -j $(nproc) shared_lib
 make install
+
+cp $OUTPUT/output/lib/librocksdb.a $OUTPUT/lib/librocksdb.a
 
 cd $BASE
--- run_rocksdb.sh
+++ run_rocksdb.sh
@@ -2,7 +2,7 @@
 go env
 
 export CGO_CFLAGS="-I$PWD/third-party/rocksdb/output/output/include"
-export CGO_LDFLAGS="-L$PWD/third-party/rocksdb/output/lib -Wl,-Bstatic -lrocksdb -lbz2 -ljemalloc -llz4 -lsnappy -lz -lzstd -Wl,-Bdynamic -lstdc++ -lm -pthread -lgomp -lrt -ldl"
+export CGO_LDFLAGS="-L$PWD/third-party/rocksdb/output/lib -Wl,-Bstatic -lrocksdb -lbz2 -llz4 -lsnappy -lz -lzstd -Wl,-Bdynamic -lstdc++ -lm -pthread -lgomp -lrt -ldl"
 
 go get github.com/ez8-lab/gorocksdb
 go test -bench 'Benchmark' | tee output.txt
