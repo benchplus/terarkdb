eadc33d12d8290dc0cf99f02f32250521f1b850b
Orca <orca.zhang@yahoo.com>
2021-12-13 15:56:41 +0800

    :bus: update time

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ export CGO_CFLAGS="-I$PWD/third-party/terarkdb/output/output/include"
 export CGO_LDFLAGS="-L$PWD/third-party/terarkdb/output/lib -Wl,-Bstatic -lrocksdb -lbz2 -ljemalloc -llz4 -lsnappy -lz -lzstd -lterark-zip-r -lboost_system -lboost_filesystem -lboost_fiber -lboost_context -Wl,-Bdynamic -lstdc++ -lm -pthread -lgomp -lrt -ldl -laio"
 
 go get github.com/ez8-lab/gorocksdb
-go test -bench 'Benchmark' -benchtime=10s | tee output.txt
+go test -bench 'Benchmark' | tee output.txt
