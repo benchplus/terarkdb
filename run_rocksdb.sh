436752aa4d5dc5d21164e9efc61abbde6be016a4
Orca <orca.zhang@yahoo.com>
2021-12-13 15:21:59 +0800

    :cow: incr time

--- run_rocksdb.sh
+++ run_rocksdb.sh
@@ -5,5 +5,5 @@ export CGO_CFLAGS="-I$PWD/third-party/rocksdb/output/output/include"
 export CGO_LDFLAGS="-L$PWD/third-party/rocksdb/output/output/lib -Wl,-Bstatic -lrocksdb -lbz2 -ljemalloc -llz4 -lsnappy -lz -lzstd -Wl,-Bdynamic -lstdc++ -lm -pthread -lgomp -lrt -ldl"
 
 go get github.com/ez8-lab/gorocksdb@rocksdb
-go test -bench 'Benchmark' | tee output.txt
+go test -bench 'Benchmark' -benchtime=60s | tee output.txt
 git stash
